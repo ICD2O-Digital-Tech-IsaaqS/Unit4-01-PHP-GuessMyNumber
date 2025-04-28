0e34f8b64bf29095b3d6c9e853b56013eb75116f
Isaaq Simon <IsaaqS>
2025-04-28 14:29:46 +0000

    Number PHP

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
   <meta name="keywords" content="Immaculata, ICD2O">
   <meta name="author" content="Isaaq Simon">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="apple-touch-icon" sizes="180x180" href="./favicon_io (13)/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./favicon_io (13)/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./favicon_io (13)/favicon-16x16.png">
-  <link rel="manifest" href="./favicon_io (13)/site.webmanifest">
+  <link rel="apple-touch-icon" sizes="180x180" href="./favicon_io (14)/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon_io (14)/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon_io (14)/favicon-16x16.png">
+  <link rel="manifest" href="./favicon_io (14)/site.webmanifest">
   <title>Guess the Number!</title>
   <!-- Link to external CSS file -->
   <link rel="stylesheet" href="./css/style.css">
